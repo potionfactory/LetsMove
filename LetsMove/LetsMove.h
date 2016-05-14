de639d10e0a7200fe181caf7d28b44685eb928ee
Matt Prowse <matt@cordlessdog.com>
2016-05-14 17:28:51 +1000

    Added initial support for installing LetsMove via Carthage.

---
+++ LetsMove/LetsMove.h
@@ -0,0 +1,19 @@
+//
+//  LetsMove.h
+//  LetsMove
+//
+//  Created by Matt Prowse on 14/05/2016.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for LetsMove.
+FOUNDATION_EXPORT double LetsMoveVersionNumber;
+
+//! Project version string for LetsMove.
+FOUNDATION_EXPORT const unsigned char LetsMoveVersionString[];
+
+#import <LetsMove/PFMoveApplication.h>
+
+
